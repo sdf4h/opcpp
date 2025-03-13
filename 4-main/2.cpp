0075c55d3d19c0d06fc1b55fe1093aa9d8c7b5a6
sdf4h <131164110+sdf4h@users.noreply.github.com>
2025-03-13 12:18:22 +0300

    Create 2.cpp

---
+++ 4-main/2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <chrono>
+#include <algorithm>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    std::vector<int> numbers(10);
+    std::ofstream outFile("numbers.txt");
+
+    for (auto& num : numbers) {
+        num = rand() % 100;
+        outFile << num << std::endl;
+    }
+    outFile.close();
+
+    std::ifstream inFile("numbers.txt");
+    numbers.clear();
+    int number;
+    while (inFile >> number) {
+        numbers.push_back(number);
+    }
+    inFile.close();
+
+    auto start = std::chrono::high_resolution_clock::now();
+    std::sort(numbers.begin(), numbers.end());
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Отсортированные числа:\n";
+    for (const int num : numbers) {
+        std::cout << num << " ";
+    }
+    std::cout << '\n';
+
+    std::cout << "Время выполнения: "
+        << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+        << " мс" << '\n';
+}
